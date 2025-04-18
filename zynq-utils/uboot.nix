6f42d857534c7eabeffbdc4d992664d013ecd434
Moritz Meier <moritz.meier@dlr.de>
2025-04-18 19:50:41 +0200

    zynq-utils: replace tfa and uboot with custom impl

--- zynq-utils/uboot.nix
+++ zynq-utils/uboot.nix
@@ -1,51 +1,100 @@
 {
+  bison,
+  buildPackages,
+  dtc,
+  flex,
+  gnutls,
   lib,
-  buildUBoot,
+  libuuid,
+  openssl,
+  pkg-config,
+  stdenv,
+  swig,
+  writeText,
   zynq-utils,
 }:
-
 {
+  defconfig ? "xilinx_zynqmp_virt_defconfig",
+  deviceTree ? "zynqmp-zcu102-rev1.0",
+  extDeviceTreeBlob ? null,
   extraConfig ? "",
-  extraEnv ? { },
-  boot2el2 ? true,
-  uboot-src ? zynq-utils.uboot-src,
+  bootToEL2 ? false,
+  ubootSrc ? zynq-utils.uboot-src,
 }:
-(buildUBoot {
-  extraMeta.platforms = [ "aarch64-linux" ];
-  defconfig = "xilinx_zynqmp_virt_defconfig";
+let
+  extra-config-file = writeText ".extra-config" extraConfig;
+in
+stdenv.mkDerivation (finalAttrs: {
+  name = "uboot-${defconfig}";
 
-  inherit extraConfig;
+  srcs = ubootSrc;
 
-  env = extraEnv;
-
-  # The `DEVICE_TREE` environment variable must only be propagated __after__ the initial
-  # `make xilinx_zynqmp_virt_defconfig` call.
-
-  preInstall = ''
-    export DEVICE_TREE="zynqmp-zcu102-rev1.0"
-  '';
-  filesToInstall = [
-    "spl/boot.bin"
-    "u-boot.elf"
-    "u-boot.img"
-    ".config"
+  nativeBuildInputs = [
+    bison
+    dtc
+    flex
+    gnutls
+    libuuid
+    openssl
+    pkg-config
+    swig
+    # https://github.com/NixOS/nixpkgs/issues/305858
+    (buildPackages.python3.withPackages (
+      pyPkgs: with pyPkgs; [
+        setuptools
+        pyelftools
+      ]
+    ))
   ];
-  version = "${uboot-src.rev}";
-  dontPatch = true; # avoid unapplicable raspberrypi patches
 
-  # u-boot-xlnx ignores the CONFIG_ARMV8_SWITCH_TO_EL1 macro, and always unconditionally
-  # boots into EL1 when doing `go`. This little patch changes that behavior to stay in
-  # EL2, so that seL4 can happily boot even in hypervisor mode.
-  prePatch = lib.strings.optionalString boot2el2 ''
-    substituteInPlace board/xilinx/zynqmp/zynqmp.c \
-      --replace armv8_switch_to_el1 armv8_switch_to_el2
+  depsBuildBuild = [ buildPackages.stdenv.cc ];
+
+  patchPhase =
+    ''
+      patchShebangs ./scripts
+      patchShebangs ./tools
+
+      sed -i 's/\/bin\/pwd/pwd/' ./Makefile
+    ''
+    + lib.strings.optionalString bootToEL2 ''
+      substituteInPlace ./board/xilinx/zynqmp/zynqmp.c \
+        --replace armv8_switch_to_el1 armv8_switch_to_el2
+    '';
+
+  configurePhase = ''
+    export KBUILD_OUTPUT=build
+    export CROSS_COMPILE=${stdenv.cc.targetPrefix}
+
+    make ${defconfig}
+
+    cat ${extra-config-file} >> $KBUILD_OUTPUT/.config
   '';
-}).overrideAttrs
-  (
-    final: prev: {
-      src = uboot-src;
-      passthru = {
-        elf = "${final.finalPackage.out}/u-boot.elf";
-      };
-    }
-  )
+
+  buildPhase = ''
+    ${lib.strings.optionalString (deviceTree != null) ''
+      export DEVICE_TREE=${deviceTree}
+    ''}
+
+    ${lib.strings.optionalString (extDeviceTreeBlob != null) ''
+      export EXT_DTB=${extDeviceTreeBlob}
+    ''}
+
+    make -j $NIX_BUILD_CORES
+  '';
+
+  installPhase = ''
+    mkdir $out
+    cp -r ./build/. $out/
+
+    mkdir $out/bin
+    cp ./build/tools/mkimage $out/bin/
+  '';
+
+  dontFixup = true;
+
+  passthru = {
+    elf = "${finalAttrs.finalPackage.out}/u-boot.elf";
+    dtb = "${finalAttrs.finalPackage.out}/u-boot.dtb";
+    config = "${finalAttrs.finalPackage.out}/.config";
+  };
+})

ae7b62b3dfaf4f13e7d8604ee6a30d038bc2df88
Moritz Meier <moritz.meier@dlr.de>
2025-04-18 19:50:41 +0200

    zynq-utils: replace tfa and uboot with custom impl

--- zynq-utils/tfa.nix
+++ zynq-utils/tfa.nix
@@ -1,25 +1,39 @@
 {
-  arm-trusted-firmware,
+  dtc,
+  stdenv,
   zynq-utils,
 }:
-
 {
-  atf-src ? zynq-utils.tfa-src,
+  plat ? "zynqmp",
+  tfa-src ? zynq-utils.tfa-src,
 }:
-(arm-trusted-firmware.buildArmTrustedFirmware {
-  platform = "zynqmp";
-  extraMakeFlags = [ "bl31" ];
-  filesToInstall = [
-    "build/zynqmp/release/bl31/bl31.elf"
-    "build/zynqmp/release/bl31.bin"
-  ];
-}).overrideAttrs
-  (
-    final: prev: {
-      src = atf-src;
+stdenv.mkDerivation (finalAttrs: {
+  name = "trusted-firmware-a-${plat}";
 
-      passthru = {
-        elf = "${final.finalPackage.out}/bl31.elf";
-      };
-    }
-  )
+  src = tfa-src;
+
+  nativeBuildInputs = [
+    dtc
+  ];
+
+  dontPatch = true;
+  dontConfigure = true;
+
+  buildPhase = ''
+    make -j $NIX_BUILD_CORES \
+      CROSS_COMPILE=${stdenv.cc.targetPrefix} \
+      PLAT=${plat} \
+      bl31
+  '';
+
+  installPhase = ''
+    mkdir $out
+    cp -r ./build/. $out/
+  '';
+
+  dontFixup = true;
+
+  passthru = {
+    elf = "${finalAttrs.finalPackage.out}/${plat}/release/bl31/bl31.elf";
+  };
+})

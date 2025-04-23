7bdca7c2ebe387548b20b76ed46fd43a6ec1ae90
Moritz Meier <moritz.meier@dlr.de>
2025-04-23 13:57:51 +0200

    add zynq-boards

---
+++ zynq-boards.nix
@@ -0,0 +1,5 @@
+final: prev: {
+  zynq-boards = {
+    te0706-0821-3be21 = prev.callPackage ./zynq-boards/te0706-0821-3be21/board.nix { };
+  };
+}
---
+++ zynq-boards/te0706-0821-3be21/board.nix
@@ -0,0 +1,36 @@
+{
+  zynq-utils,
+}:
+zynq-utils.zynqmp-board {
+  name = "te0706-0821-3be21";
+  src = ./vivado-srcs;
+
+  sdt = {
+    extraDts = [ ./dts/qspi-nor-flash.dts ];
+  };
+
+  uboot = {
+    extraConfig = ''
+      CONFIG_ENV_IS_NOWHERE=n
+      CONFIG_ENV_IS_IN_FAT=n
+      CONFIG_ENV_IS_IN_NAND=n
+
+      CONFIG_TFTP_PORT=y
+
+      CONFIG_LOG=y
+      CONFIG_CMD_LOG=y
+      CONFIG_LOG_DEFAULT_LEVEL=4
+      CONFIG_LOG_MAX_LEVEL=7
+      CONFIG_LOG_CONSOLE=y
+    '';
+  };
+
+  boot-jtag-cmd = {
+    forceBootModeJtag = true;
+  };
+
+  flash-qspi-cmd = {
+    flashType = "qspi-x8-dual_parallel";
+    flashDensity = 128;
+  };
+}

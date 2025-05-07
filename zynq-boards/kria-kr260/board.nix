14e4c33c04526f5480b30cf588b0025704f34d6b
Moritz Meier <moritz.meier@dlr.de>
2025-05-08 00:09:18 +0200

    zynq-boards: add board.dtsi for kria-kr260

--- zynq-boards/kria-kr260/board.nix
+++ zynq-boards/kria-kr260/board.nix
@@ -9,24 +9,28 @@ zynq-utils.zynqmp.board {
   };
 
   linux-dt = {
-    #extraDtsi = ./dts/board.dtsi;
+    extraDtsi = ./dts/board.dtsi;
   };
 
   tfa.extraMakeFlags = [ "ZYNQMP_CONSOLE=cadence1" ];
 
   uboot = {
     extraConfig = ''
-      # CONFIG_ENV_IS_NOWHERE=n
-      # CONFIG_ENV_IS_IN_FAT=n
-      # CONFIG_ENV_IS_IN_NAND=n
+      CONFIG_ENV_IS_NOWHERE=n
+      CONFIG_ENV_IS_IN_FAT=n
+      CONFIG_ENV_IS_IN_NAND=n
+      CONFIG_ENV_SIZE=0x20000
+      CONFIG_ENV_SECT_SIZE=0x20000
+      CONFIG_ENV_OFFSET=0x2200000
+      CONFIG_ENV_OFFSET_REDUND=0x2220000
 
-      # CONFIG_TFTP_PORT=y
+      CONFIG_USB_ONBOARD_HUB=y
 
-      # CONFIG_LOG=y
-      # CONFIG_CMD_LOG=y
-      # CONFIG_LOG_DEFAULT_LEVEL=4
-      # CONFIG_LOG_MAX_LEVEL=7
-      # CONFIG_LOG_CONSOLE=y
+      CONFIG_LOG=y
+      CONFIG_CMD_LOG=y
+      CONFIG_LOG_DEFAULT_LEVEL=4
+      CONFIG_LOG_MAX_LEVEL=7
+      CONFIG_LOG_CONSOLE=y
     '';
   };
 
@@ -37,5 +41,9 @@ zynq-utils.zynqmp.board {
   flash-qspi = {
     flashType = "qspi-x4-single";
     flashDensity = 64;
+
+    extraArgs = [
+      "-offset 0x00200000"
+    ];
   };
 }

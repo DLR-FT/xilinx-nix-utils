8a49ce49a489e7566d345b34a4e15595339be984
Moritz Meier <moritz.meier@dlr.de>
2025-07-25 18:30:54 +0200

    move xilinx-lab into seperate overlay

---
+++ xilinx-lab.nix
@@ -0,0 +1,19 @@
+final: prev: {
+  xilinx-lab-unwrapped = final.xilinx-lab-2024-2-unwrapped;
+  xilinx-lab = final.xilinx-lab-2024-2;
+
+  xilinx-lab-2024-2-unwrapped = final.callPackage ./xilinx-unified/xilinx-unified.nix rec {
+    name = "xilinx-lab";
+    version = "2024.2_1113_1001";
+    installTar = final.requireFile {
+      name = "Vivado_Lab_Lin_${version}.tar";
+      url = "https://www.xilinx.com/";
+      hash = "sha256-2ONg2GWi9vbt7bK7NfpPiaJLM29Glot8/94oP2Rp+zg=";
+    };
+    install_config = null;
+  };
+
+  xilinx-lab-2024-2 = final.callPackage ./xilinx-unified/wrap-xilinx.nix {
+    inputDerivation = final.xilinx-lab-2024-2-unwrapped;
+  };
+}

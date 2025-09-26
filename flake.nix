86399097151d0ad41248f010f3c39a3d1ea14f41
Sven Friedrich <sven@autumnal.de>
2025-09-26 15:26:26 +0200

    chore: flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/50ab793786d9de88ee30ec4e4c24fb4236fc2674' (2025-06-30)
      → 'github:NixOS/nixpkgs/d1d883129b193f0b495d75c148c2c3a7d95789a0' (2025-09-23)
    • Updated input 'treefmt':
        'github:numtide/treefmt-nix/421b56313c65a0815a52b424777f55acf0b56ddf' (2025-07-20)
      → 'github:numtide/treefmt-nix/5eda4ee8121f97b218f7cc73f5172098d458f1d1' (2025-09-24)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix wrapper for the Xilinx Unified Toolchain and additional utilities for using Nix as a build system for Zynq firmware";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
     treefmt.url = "github:numtide/treefmt-nix";

90ac7f7704ac2ecfc8788556b10534107aa4141a
Moritz Meier <moritz.meier@dlr.de>
2025-12-16 20:03:12 +0100

    remove other versions

--- xilinx-lab.nix
+++ xilinx-lab.nix
@@ -1,58 +1,27 @@
-final: prev: {
-  xilinx-lab = final.xilinx-lab-versions.default.xilinx-lab;
-
+final: prev:
+let
+  meta = rec {
+    baseName = "xilinx-lab";
+    version = "2024.2_1113_1001";
+    installTar = final.requireFile {
+      name = "Vivado_Lab_Lin_${version}.tar";
+      url = "https://www.xilinx.com/";
+      hash = "sha256-2ONg2GWi9vbt7bK7NfpPiaJLM29Glot8/94oP2Rp+zg=";
+    };
+    installConfig = ./xilinx-pkgs/install-configs/xlnx-lab-2024-2.txt;
+  };
+in
+{
   # Provide xilinx-unified-or-lab, if it does not already exist
   xilinx-unified-or-lab =
     if (prev ? xilinx-unified-or-lab) then prev.xilinx-unified-or-lab else final.xilinx-lab;
 
-  xilinx-lab-versions = {
-    default = final.xilinx-lab-versions."2024.2";
-
-    "2025.1" =
-      let
-        args = rec {
-          baseName = "xilinx-lab";
-          version = "2025.1_0530_0145";
-          installTar = final.requireFile {
-            name = "Vivado_Lab_Lin_${version}.tar";
-            url = "https://www.xilinx.com/";
-            hash = "sha256-2VogZ3cb1nUNu0Nv20NVuxTZJp9fwkwAZbTJG8av86k=";
-          };
-          installConfig = ./xilinx-pkgs/install-configs/xlnx-lab-2025-1.txt;
-        };
-      in
-      {
-        install-config = final.xilinx-lab-utils.genInstallConfig args;
-
-        xilinx-lab-unwrapped = final.xilinx-lab-utils.install args;
-        xilinx-lab = final.xilinx-lab-utils.wrap {
-          inputDerivation = final.xilinx-lab-versions."2025.1".xilinx-lab-unwrapped;
-        };
-      };
-
-    "2024.2" =
-      let
-        args = rec {
-          baseName = "xilinx-lab";
-          version = "2024.2_1113_1001";
-          installTar = final.requireFile {
-            name = "Vivado_Lab_Lin_${version}.tar";
-            url = "https://www.xilinx.com/";
-            hash = "sha256-2ONg2GWi9vbt7bK7NfpPiaJLM29Glot8/94oP2Rp+zg=";
-          };
-          installConfig = ./xilinx-pkgs/install-configs/xlnx-lab-2024-2.txt;
-        };
-      in
-      {
-        install-config = final.xilinx-lab-utils.genInstallConfig args;
-
-        xilinx-lab-unwrapped = final.xilinx-lab-utils.install args;
-        xilinx-lab = final.xilinx-lab-utils.wrap {
-          inputDerivation = final.xilinx-lab-versions."2024.2".xilinx-lab-unwrapped;
-          extraTargetPkgs = pkgs: [ pkgs.libxcrypt-legacy ];
-        };
-      };
+  xilinx-lab = final.xilinx-lab-utils.wrap {
+    inputDerivation = final.xilinx-lab-unwrapped;
+    extraTargetPkgs = pkgs: [ pkgs.libxcrypt-legacy ];
   };
+
+  xilinx-lab-unwrapped = final.xilinx-lab-utils.install meta;
 
   xilinx-lab-utils = {
     genInstallConfig = final.callPackage ./xilinx-pkgs/install.nix {
